d9e83d9ef2904b6814ed7557ffa553cdb4b81af9
JoHyukJun <computer@khu.ac.kr>
2022-06-15 19:24:25 +0900

    error handling

--- apiprj/run-init-dev.sh
+++ apiprj/run-init-dev.sh
@@ -3,6 +3,8 @@
 find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc" -delete
 
+python manage.py makemigrations api
+python manage.py migrate api
 python manage.py makemigrations
 python manage.py migrate
 
